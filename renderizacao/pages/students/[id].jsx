833a4ac7b4067d429c14f45641abd5f567f1b71c
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-11 16:15:31 -0300

    Usando getStaticPaths - Parte 2

--- renderizacao/pages/students/[id].jsx
+++ renderizacao/pages/students/[id].jsx
@@ -1,36 +1,43 @@
-export function getStaticPaths() { 
+export async function getStaticPaths() { 
+    const resp = await fetch('http://localhost:3000/api/students/tutors');
+    const ids = await resp.json();
+
+    const paths = ids.map(id => {
+        return {
+            params: {
+                id: `${ id }`,
+            },
+        };
+    });
+
     return {
         fallback: false,
-        paths: [
-            { 
-                params: {
-                    id:'1',
-                },
-            },
-            { 
-                params: {
-                    id:'2',
-                },
-            },
-            { 
-                params: {
-                    id:'3',
-                },
-            },
-        ],
+        paths,
     };
 };
 
-export function getStaticProps() { 
+export async function getStaticProps({ params }) { 
+    const resp = await fetch(`http://localhost:3000/api/students/${ params.id }`);
+    const student = await resp.json();
+    
     return {
-        props: {},
+        props: {
+            student,
+        },
     };
 };
 
-export default function StudentById() {
+export default function StudentById(props) {
+    const { student } = props;
+
     return (
         <div>
             <h1>Detalhes do Aluno</h1>
+            <ul>
+                <li>{ student.id }</li>
+                <li>{ student.name }</li>
+                <li>{ student.email }</li>
+            </ul>
         </div>
     );
 };
