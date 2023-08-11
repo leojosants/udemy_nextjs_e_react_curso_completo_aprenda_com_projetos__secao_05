162fca7630b658eb331bfc411a61fd0e38d0af2c
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-11 00:33:21 -0300

    Componente Estático #02

---
+++ renderizacao/pages/static2.jsx
@@ -0,0 +1,16 @@
+export function getStaticProps() {
+	return {
+		props: {
+			number: Math.random(),
+		},
+	};
+};
+
+export default function Static2(props) {
+	return (
+		<div>
+			<h1>Estático #02</h1>
+			<h2>{props.number}</h2>
+		</div>
+	);
+};
