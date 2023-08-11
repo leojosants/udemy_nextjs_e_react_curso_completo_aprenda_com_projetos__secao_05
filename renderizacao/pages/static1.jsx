06b0fef6c595ef609bc55fb7954a12ba6e0906ff
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-11 00:16:27 -0300

    Componente Estático #01

---
+++ renderizacao/pages/static1.jsx
@@ -0,0 +1,16 @@
+// import { useEffect, useState } from 'react';
+
+export default function Static1() {
+	// const [number, setNumber] = useState(0);
+
+	// useEffect(() => {
+	// 	setNumber(Math.random())
+	// }, []);
+
+	return (
+		<div>
+			<h1>Estático #01</h1>
+			{/* <h2>{number}</h2> */}
+		</div>
+	);
+}
